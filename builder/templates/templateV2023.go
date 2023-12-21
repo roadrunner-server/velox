cfa3f4fdd5fe1bdece39f07c0feccacc09787cad
Valery Piashchynski <piashchynski.valery@gmail.com>
2023-12-21 15:46:08 +0100

    fix: exclude viper 1.18.0/1
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- builder/templates/templateV2023.go
+++ builder/templates/templateV2023.go
@@ -5,24 +5,30 @@ module github.com/roadrunner-server/roadrunner/{{.ModuleVersion}}
 
 go 1.21
 
-toolchain go1.21.1
+toolchain go1.21.5
 
 require (
         github.com/buger/goterm v1.0.4
         github.com/dustin/go-humanize v1.0.1
         github.com/joho/godotenv v1.5.1
         github.com/olekukonko/tablewriter v0.0.5
-        github.com/spf13/cobra v1.7.0
-		github.com/spf13/viper v1.17.0
+        github.com/spf13/cobra v1.8.0
+	github.com/spf13/viper v1.18.2
         github.com/stretchr/testify v1.8.2
-		go.uber.org/automaxprocs v1.5.2
-		github.com/roadrunner-server/informer/v4 latest
-		github.com/roadrunner-server/resetter/v4 latest
-		github.com/roadrunner-server/config/v4 latest
+	go.uber.org/automaxprocs v1.5.2
+	github.com/roadrunner-server/informer/v4 latest
+	github.com/roadrunner-server/resetter/v4 latest
+	github.com/roadrunner-server/config/v4 latest
 
-		// Go module pseudo-version
-		{{range $v := .Entries}}{{$v.Module}} {{$v.PseudoVersion}}
-		{{end}}
+	// Go module pseudo-version
+	{{range $v := .Entries}}{{$v.Module}} {{$v.PseudoVersion}}
+	{{end}}
+)
+
+exclude (
+	github.com/spf13/viper v1.18.0
+	github.com/spf13/viper v1.18.1
+	go.temporal.io/api v1.26.1
 )
 
 replace (
